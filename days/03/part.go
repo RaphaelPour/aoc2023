c03a7b3e3fcb67bb263a174eea6afe44d094db70
Raphael Pour <info@raphaelpour.de>
2023-12-03 20:19:27 +0100

    03: solve part 2

--- days/03/part.go
+++ days/03/part.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/RaphaelPour/stellar/input"
 	"github.com/RaphaelPour/stellar/math"
@@ -19,11 +18,12 @@ const (
 )
 
 type Cell struct {
-	buffer    string
-	num       int
-	kind      CellKind
-	connected bool
-	coord     Point
+	buffer      string
+	num         int
+	kind        CellKind
+	connected   bool
+	coord       Point
+	symbolCoord Point
 }
 
 func NewCell(in string, coord Point) Cell {
@@ -112,10 +112,10 @@ func (g *Grid) Set(x, y int, cell Cell) {
 	g.data[Point{x, y}] = cell
 }
 
-func (g Grid) HasConnections(p, origin Point, depth int) bool {
+func (g Grid) HasConnections(p, origin Point, depth int) (Point, bool) {
 	if g.Get(p).kind == CELL_KIND_SYMBOL {
-		fmt.Printf("%s %s SYMB\n", strings.Repeat(" ", depth), p)
-		return true
+		// fmt.Printf("%s %s SYMB\n", strings.Repeat(" ", depth), p)
+		return p, true
 	}
 
 	for x := -1; x <= 1; x++ {
@@ -132,8 +132,8 @@ func (g Grid) HasConnections(p, origin Point, depth int) bool {
 			}
 
 			if cell.kind == CELL_KIND_SYMBOL {
-				fmt.Printf("%s %s Neighbor SYMB\n", strings.Repeat(" ", depth), p)
-				return true
+				// fmt.Printf("%s %s Neighbor SYMB\n", strings.Repeat(" ", depth), p)
+				return cell.coord, true
 			}
 
 			// only allow positive number nieghbor lookup so numbers
@@ -143,21 +143,21 @@ func (g Grid) HasConnections(p, origin Point, depth int) bool {
 			}*/
 
 			if cell.kind == CELL_KIND_NUMBER {
-				fmt.Printf("%s %s check number neighbor\n", strings.Repeat(" ", depth), p)
-				con := g.HasConnections(cell.coord, p, depth+1)
+				// fmt.Printf("%s %s check number neighbor\n", strings.Repeat(" ", depth), p)
+				symbolCoord, con := g.HasConnections(cell.coord, p, depth+1)
 				if con {
-					fmt.Printf("%s %s deep neighbor SYMB\n", strings.Repeat(" ", depth), p)
-					return con
+					// fmt.Printf("%s %s deep neighbor SYMB\n", strings.Repeat(" ", depth), p)
+					return symbolCoord, con
 				}
 			}
 		}
 	}
 
-	fmt.Printf("%s %s nope\n", strings.Repeat(" ", depth), p)
-	return false
+	//  fmt.Printf("%s %s nope\n", strings.Repeat(" ", depth), p)
+	return Point{}, false
 }
 
-func (g Grid) FindMissing() int {
+func (g Grid) FindMissing() (int, int) {
 	cells := make([]Cell, 0)
 
 	for y := 0; y <= g.maxY; y++ {
@@ -166,24 +166,37 @@ func (g Grid) FindMissing() int {
 			if !ok || cell.kind != CELL_KIND_NUMBER {
 				continue
 			}
-			fmt.Println(cell.buffer)
-			if g.HasConnections(Point{x, y}, Point{x, y}, 0) {
+			// fmt.Println(cell.buffer)
+			if symbolCoord, ok := g.HasConnections(Point{x, y}, Point{x, y}, 0); ok {
+				cell.symbolCoord = symbolCoord
 				cells = append(cells, cell)
-				fmt.Print(">", cell.buffer, "<\n")
+				fmt.Print(">", cell.buffer, " ", symbolCoord, "<\n")
 			}
 		}
 	}
 
+	symbolCache := make(map[Point][]int)
+
 	sum := 0
 	buffer := cells[0].buffer
 	for i, cell := range cells[1:] {
-		fmt.Printf("x=%d y=%d\n", cell.coord.x, cell.coord.y)
+		// fmt.Printf("x=%d y=%d %s\n", cell.coord.x, cell.coord.y, cell.symbolCoord)
 		if cell.coord.y != cells[i].coord.y ||
 			math.Abs(cell.coord.x-cells[i].coord.x) > 1 {
 			num := stellar_strings.ToInt(buffer)
 			fmt.Println(num)
 			sum += num
 			buffer = ""
+
+			if _, ok := symbolCache[cells[i].symbolCoord]; !ok {
+				symbolCache[cells[i].symbolCoord] = make([]int, 0)
+			}
+
+			symbCell, ok := g.data[cells[i].symbolCoord]
+			fmt.Println(num, symbCell.buffer, cells[i].symbolCoord)
+			if ok && symbCell.buffer == "*" {
+				symbolCache[cells[i].symbolCoord] = append(symbolCache[cells[i].symbolCoord], num)
+			}
 		}
 
 		buffer += cell.buffer
@@ -192,15 +205,32 @@ func (g Grid) FindMissing() int {
 	fmt.Println(num)
 	sum += num
 
-	return sum
+	if _, ok := symbolCache[cells[len(cells)-1].symbolCoord]; !ok {
+		symbolCache[cells[len(cells)-1].symbolCoord] = make([]int, 0)
+	}
+	symbolCache[cells[len(cells)-1].symbolCoord] = append(symbolCache[cells[len(cells)-1].symbolCoord], num)
+
+	sum2 := 0
+	fmt.Println(symbolCache)
+	for _, numbers := range symbolCache {
+		if len(numbers) != 2 {
+			continue
+		}
+		sum2 += numbers[0] * numbers[1]
+	}
+
+	return sum, sum2
 }
 
-func part1(data []string) int {
+func part1(data []string) (int, int) {
 	grid := Grid{make(map[Point]Cell), -1, -1}
 
 	for y, line := range data {
 		for x, r := range line {
 			ch := string(r)
+			if ch == "." {
+				continue
+			}
 			grid.Set(x, y, NewCell(ch, Point{x, y}))
 		}
 	}
@@ -220,8 +250,9 @@ func main() {
 	// data := input.LoadInt("input")
 
 	fmt.Println("== [ PART 1 ] ==")
-	fmt.Println(part1(data))
+	p1, p2 := part1(data)
+	fmt.Println(p1)
 
-	// fmt.Println("== [ PART 2 ] ==")
-	// fmt.Println(part2(data))
+	fmt.Println("== [ PART 2 ] ==")
+	fmt.Println(p2)
 }
