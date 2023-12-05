2aa566e0db4fdcee1d4ae55183c702af6429e79c
Raphael Pour <info@raphaelpour.de>
2023-12-05 12:36:07 +0100

    05: add test

--- days/05/part_test.go
+++ days/05/part_test.go
@@ -7,6 +7,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestFindKey(t *testing.T) {
+	m, err := NewMap(input.LoadString("input1")[2:])
+	require.NoError(t, err)
+
+	require.Equal(t, m.FindKey("seed"), Key{from: "seed", to: "soil"})
+}
+
 func TestPart1Example(t *testing.T) {
+	t.Skip()
 	require.Equal(t, 35, part1(input.LoadString("input1")))
 }
