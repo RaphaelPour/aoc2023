9390167c3db599146bad777c895bcdee92e9c3d0
Raphael Pour <info@raphaelpour.de>
2023-12-14 14:54:00 +0100

    14: add tests

--- days/14/part_test.go
+++ days/14/part_test.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-    "testing"
+	"testing"
 
-        "github.com/stretchr/testify/require"
+	"github.com/RaphaelPour/stellar/input"
+	"github.com/stretchr/testify/require"
 )
 
-func TestExample(t *testing.T) {
-    // require.Equal(t, 0, part1([]string{})
+func TestExamplePart1(t *testing.T) {
+	require.Equal(t, 136, part1(input.LoadString("input1")))
+}
+
+func TestExamplePart2(t *testing.T) {
+	require.Equal(t, 64, part2(input.LoadString("input1")))
+}
+
+func TestInputPart1(t *testing.T) {
+	require.Equal(t, 109665, part1(input.LoadString("input")))
 }
