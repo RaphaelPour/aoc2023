0bf74c1b01dd517bba62f642988babb260238e95
Raphael Pour <info@raphaelpour.de>
2023-12-15 23:29:35 +0100

    test: add expand test

--- days/10/part_test.go
+++ days/10/part_test.go
@@ -16,6 +16,25 @@ func TestExample1Part1(t *testing.T) {
 	require.Equal(t, 4, part1(input.LoadString("input1")))
 }
 
+func TestExpand(t *testing.T) {
+	input := []string{
+		"S-7",
+		"|.|",
+		"L-J",
+	}
+
+	m := NewMap(input)
+	require.Equal(t, P{0, 0}, m.start)
+	require.Equal(t, 3, m.w)
+	require.Equal(t, 3, m.h)
+
+	m.Expand()
+
+	require.Equal(t, P{1, 1}, m.start)
+	require.Equal(t, 9, m.w)
+	require.Equal(t, 9, m.h)
+}
+
 func TestExample2Part2(t *testing.T) {
 	require.Equal(t, 4, part2(input.LoadString("input2")))
 }
