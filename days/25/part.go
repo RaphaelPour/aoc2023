0cb3dced0490c93c5d37ddabc1fdbd0985199ac8
Raphael Pour <info@raphaelpour.de>
2023-12-27 14:37:42 +0100

    25: try try try

--- days/25/part.go
+++ days/25/part.go
@@ -2,183 +2,209 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sort"
 	"strings"
+	"time"
 
 	"github.com/RaphaelPour/stellar/input"
 )
 
-type Component struct {
-	name string
-	adj  map[string]struct{}
+/* Histogram
+== [ PART 1 ] ==
+299900 99.97
+lnr 53463
+pgt 53438
+tjz 51153
+vph 50967
+zkt 48092
+jhq 47707
+pvv 29416
+fqm 28537
+mqd 28513
+rmr 27019
+pvz 23430
+jzd 22611
+gfq 20513
+jzp 18219
+tph 17941
+ggn 17933
+ssx 17327
+qss 17249
+djr 16290
+cfc 15929
+0
+11m37.302033634s
+*/
+
+type Pair struct {
+	Key   string
+	Value int
 }
 
-func (c Component) String() string {
-	return fmt.Sprintf("%s -> %v", c.name, c.adj)
+func (p Pair) String() string { return fmt.Sprintf("%3s %4d", p.Key, p.Value) }
+
+type Pairs []Pair
+
+func (p Pairs) Len() int           { return len(p) }
+func (p Pairs) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p Pairs) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+type Graph struct {
+	edges map[string][]string
+	hist  map[string]int
 }
 
-type Diagram struct {
-	components map[string]Component
-	candidates []string
-}
+func NewGraph(data []string) Graph {
+	g := Graph{
+		edges: make(map[string][]string),
+		hist:  make(map[string]int),
+	}
 
-func NewDiagram(data []string) Diagram {
-	d := Diagram{}
-	d.components = make(map[string]Component)
 	for _, line := range data {
 		parts := strings.Split(line, " ")
-		name := parts[0][:3]
-		c := Component{
-			name: name,
-			adj:  map[string]struct{}{},
+		from := parts[0][0:3]
+		to := parts[1:]
+
+		// add from + neighbor
+		if _, ok := g.edges[from]; !ok {
+			g.edges[from] = make([]string, 0)
+		}
+		for _, neighbor := range to {
+			g.edges[from] = append(g.edges[from], neighbor)
 		}
 
-		for _, n := range parts[1:] {
-			c.adj[n] = struct{}{}
-		}
-		d.components[name] = c
-	}
-
-	for name, c := range d.components {
-		if name == "" {
-			fmt.Println("init err")
-		}
-		for neighbor := range c.adj {
-			if neighbor == "" {
-				fmt.Println("init err2")
+		// add from to each neighbor
+		for _, neighbor := range to {
+			if _, ok := g.edges[neighbor]; !ok {
+				g.edges[neighbor] = make([]string, 0)
 			}
-			c2 := d.components[neighbor]
-			if len(c2.adj) == 0 {
-				c2.adj = make(map[string]struct{})
-			}
-			c2.adj[name] = struct{}{}
-			d.components[neighbor] = c2
+			g.edges[neighbor] = append(g.edges[neighbor], from)
 		}
 	}
 
-	d.candidates = make([]string, 0)
-	for _, c := range d.components {
-		d.candidates = append(d.candidates, c.name)
-	}
-
-	return d
+	return g
 }
 
-func (d Diagram) Find(current, goal string, visited, hist map[string]int) bool {
-	if current == goal {
-		// fmt.Printf("%q", current)
-		hist[current] = hist[current] + 1
+func (g Graph) Find(node, goal string, visited map[string]struct{}, maxDepth, depth int) bool {
+	if depth == maxDepth {
+		return false
+	}
+
+	if node == goal {
+		visited[node] = struct{}{}
+		g.hist[node] = g.hist[node] + 1
 		return true
 	}
 
-	if current == "" {
-		fmt.Println("DONG DONG DONG")
+	if _, ok := visited[node]; ok {
+		return false
 	}
-	visited[current] = 0
 
-	for n := range d.components[current].adj {
-		if _, ok := visited[n]; ok {
-			continue
-		}
-		if d.Find(n, goal, visited, hist) {
+	visited[node] = struct{}{}
+	for _, n := range g.edges[node] {
+		if g.Find(n, goal, visited, maxDepth, depth+1) {
+			g.hist[n] = g.hist[n] + 1
 			return true
 		}
 	}
 	return false
 }
 
-type Element struct {
-	name   string
-	visits int
+type Edge struct {
+	from, to string
 }
 
-func (e Element) String() string {
-	return fmt.Sprintf("%d %s", e.visits, e.name)
-}
-
-type Elements struct {
-	list []Element
-}
-
-func (e Elements) Len() int {
-	return len(e.list)
-}
-
-func (e Elements) Less(i, j int) bool {
-	return e.list[i].visits < e.list[j].visits
-}
-
-func (e Elements) Swap(i, j int) {
-	e.list[i], e.list[j] = e.list[j], e.list[i]
-}
-
-func ElementsFromHist(hist map[string]int) Elements {
-	e := Elements{
-		list: make([]Element, len(hist)),
+func (g Graph) FindDividerEdges(candidates []string) []Edge {
+	e := make([]Edge, 0)
+	for i, c := range candidates {
+		e = append(e, Edge{
+			from: c,
+			to: g.FindNearest(
+				c,
+				append(append([]string{}, candidates[:i], candidates[i+1:])),
+			),
+		},
+		)
 	}
-	i := 0
-	for name, visits := range hist {
-		if name == "" {
-			fmt.Println("DING DING DING")
-		}
-		e.list[i] = Element{name: name, visits: visits}
-		i++
-	}
-
-	sort.Sort(e)
-
 	return e
 }
 
-func (d Diagram) FindAll() map[string]int {
-	hist := make(map[string]int)
+func (g Graph) FindNearest(node, candidates []string) string {
+	steps := 1000
+	best := ""
 
-	// start := time.Now()
+	for _, c := range candidates {
+		depth := 1
+		for {
+			if g.Find(node, c, map[string]struct{}{}, depth, 0) {
+				break
+			}
+			depth++
+		}
+	}
+	return best
+}
 
-	for i := 0; i < len(d.candidates)-1; i++ {
-		/*
-			fmt.Printf(
-				"\r%d/%d (%f) ETA %.2fs",
-				i,
-				len(d.candidates),
-				100.0/float64(len(d.candidates))*float64(i),
-				time.Since(start).Seconds()/float64(i)*float64((len(d.candidates)-i)),
-			)*/
-		for j := i + 1; j < len(d.candidates); j++ {
-			d.Find(
-				d.candidates[i],
-				d.candidates[j],
-				map[string]int{},
-				hist,
-			)
+func (g Graph) CreateHist() {
+	keys := make([]string, len(g.edges))
+	i := 0
+	for key := range g.edges {
+		keys[i] = key
+		i++
+	}
+
+	rounds := 50000
+	for i := 0; i < rounds; i++ {
+		if i%100 == 0 {
+			fmt.Printf("\r%d %.2f", i, 100.0/float64(rounds)*float64(i))
+		}
+		a := keys[rand.Intn(len(keys))]
+		b := keys[rand.Intn(len(keys))]
+		for depth := 1; ; depth++ {
+			if g.Find(a, b, map[string]struct{}{}, depth, 0) {
+				break
+			}
 		}
 	}
 	fmt.Println("")
-	return hist
+}
+
+func (g Graph) PrintEdges() {
+	for edge, neighbors := range g.edges {
+		fmt.Printf("%s -> %v\n", edge, neighbors)
+	}
+}
+
+func (g Graph) PrintHist() {
+	pairs := make(Pairs, len(g.hist))
+
+	i := 0
+	for node, count := range g.hist {
+		pairs[i] = Pair{Key: node, Value: count}
+		i++
+	}
+
+	sort.Sort(sort.Reverse(pairs))
+
+	for i := 0; i < len(pairs) && i < 20; i++ {
+		fmt.Printf("%s %d\n", pairs[i].Key, pairs[i].Value)
+	}
 }
 
 func part1(data []string) int {
-	d := NewDiagram(data)
-	hist := d.FindAll()
-
-	e := ElementsFromHist(hist)
-	for _, e := range e.list[0:7] {
-		fmt.Println(e)
-	}
-
-	return len(hist)
-}
-
-func part2(data []string) int {
+	g := NewGraph(data)
+	g.CreateHist()
+	g.PrintHist()
+	fmt.Println(g.FindDividerEdges())
 	return 0
 }
 
 func main() {
-	data := input.LoadString("input1")
+	start := time.Now()
+	data := input.LoadString("input")
 
 	fmt.Println("== [ PART 1 ] ==")
 	fmt.Println(part1(data))
-
-	// fmt.Println("== [ PART 2 ] ==")
-	// fmt.Println(part2(data))
+	fmt.Println(time.Since(start))
 }
